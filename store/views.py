793dd2cedad3c52010045371e688fa6b600c65e6
Amegbor Emmanuel Kwadwo <emmanuelamegbor26@gmail.com>
2026-05-19 20:33:31 +0000

    Added product detail  page

--- store/views.py
+++ store/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from .models import product
+from .models import Product
+
 
 def product_list(request):
-    products = product.objects.all()
+    products = Product.objects.all()
     return render(request, 'store/product_list.html', {'products': products})
-    
-    
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'store/product_detail.html', {'product': product})
